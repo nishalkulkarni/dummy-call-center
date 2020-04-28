62d065761370cafebc82554b891234d34c35644c
Nishal Kulkarni <kulknishu@gmail.com>
2020-04-28 23:59:04 +0530

    Add current status function

--- call_center.cpp
+++ call_center.cpp
@@ -1,48 +1,113 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class CallCenter{
+class CallCenter {
     private:
-        queue<pair<int,string>> waiting_list;
-        queue<pair<int,string>>* desks;
-        bool* occupied;
-        vector<string> employees;
-        int num_employee;
-    public:
+    queue<string> waiting_list;
+    queue<string>* desks;
+    bool* occupied;
+    vector<string> employees;
+    int num_employee;
+    int max_queue_size;
 
-    CallCenter(int num_employee){
+    public:
+    CallCenter(int num_employee, int max_queue_size) {
         this->num_employee = num_employee;
-        desks = new queue<pair<int,string>>[num_employee];
+        this->max_queue_size = max_queue_size;
+
+        desks = new queue<string>[num_employee];
         occupied = new bool[num_employee];
 
-        for(int i=0;i<num_employee;i++){
-            cout<<occupied[i]<<" ";
-        }cout<<endl;
-        
+        for (int i = 0; i < num_employee; i++) {
+            cout << occupied[i] << " ";
+        }
+        cout << endl;
+
         getEmployeeInfo();
-        cout<<"Allah"<<endl;
+        cout << "Allah" << endl;
     }
 
-    void getEmployeeInfo(){
-        cout<<"Enter names of all the employees : "<<endl;
-        for(int i=0;i<num_employee;i++){
+    void getEmployeeInfo() {
+        cout << "Enter names of all the employees : " << endl;
+        for (int i = 0; i < num_employee; i++) {
             string name;
-            cin>>name;
+            cin >> name;
             employees.push_back(name);
         }
     }
 
+    void newCall() {
+        string name;
+        cout << "Enter name of caller : " << endl;
+        cin >> name;
+    }
+    void endCall() {
+        string name;
+        cout << "Enter name of the caller to end call : " << endl;
+        cin >> name;
+
+        for (int i = 0; i < num_employee; i++) {
+        }
+    }
+    void currentStatus() {
+        for (int i = 0; i < num_employee; i++) {
+            cout << "\nDesk #" << i + 1 << " attended by " << employees[i] << endl;
+            if (desks[i].size() == 0) {
+                cout << "Status: Free" << endl;
+            } else {
+                cout << "Status: Busy" << endl;
+                queue<string> temp;
+                temp = desks[i];
+                cout << "Currently Attending: ";
+                cout << temp.front() << endl;
+                temp.pop();
+                cout << "In the waiting list: " << endl;
+                while (temp.size() != 0) {
+                    cout << temp.front() << endl;
+                    temp.pop();
+                }
+            }
+            cout << endl;
+        }
+    }
 };
 
+int main() {
+    CallCenter center(4, 3);
 
-int main(){
-
-    CallCenter center(4);
+    ofstream logfile;
+    logfile.open("log.txt");
 
     int choice = 0;
-    while(choice != 4){
-        
+    while (choice != 4) {
+        cout << "Call center running..." << endl;
+        cout << "1. New Call" << endl;
+        cout << "2. End Call" << endl;
+        cout << "3. View Current Status" << endl;
+        cout << "4. Shutdown Call center" << endl;
+
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                center.newCall();
+                break;
+            case 2:
+                center.endCall();
+                break;
+            case 3:
+                center.currentStatus();
+                break;
+            case 4:
+                logfile << "Writing this to a file.\n";
+                logfile.close();
+                return 0;
+                break;
+            default:
+                cout << "Invalid Choice. Try again." << endl;
+                break;
+        }
     }
 
     return 0;
